d58aa13ed4595603682806761ce30440f5e10116
Richard Körber <github@shredzone.org>
2020-05-28 19:52:19 +0200

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+/*
+ * Shredzone Commons - suncalc
+ *
+ * Copyright (C) 2020 Richard "Shred" Körber
+ *   http://commons.shredzone.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * Module definition for commons-suncalc.
+ */
+module org.shredzone.commons.suncalc {
+    requires static com.github.spotbugs.annotations;
+
+    exports org.shredzone.commons.suncalc;
+    exports org.shredzone.commons.suncalc.param;
+}
